ef09d012b8eb9a6d79340bc1b1f3fddf3e57d15d
UdaraBGAkila <gamageudara77@gmail.com>
2016-10-12 22:59:34 +0530

    computing main

---
+++ computing.php
@@ -0,0 +1,13 @@
+<?php
+include_once("header.php");
+?>
+<style> a {text-decoration: none; }</style>
+<br><br><br><br>
+<a href="year/1.php"> 1st year </a><br>
+<a href="year/2.php"> 2nd year </a><br>
+<a href="year/3.php"> 3rd year </a><br>
+<a href="year/4.php"> 4th year </a>
+<br><br><br><br>
+<?php
+include_once("footer.php");
+?>
