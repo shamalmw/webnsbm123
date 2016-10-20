9e9b88cb09e3e6e3460a598ecc8261cb3b5c34bd
UdaraBGAkila <gamageudara77@gmail.com>
2016-10-20 23:47:45 +0530

    Update Management.php

--- Management.php
+++ Management.php
@@ -3,10 +3,10 @@ include_once("headerm.php");
 ?>
 <style> a {text-decoration: none; }</style>
 <br><br><br><br>
-<a href="year/1.php"> 1st year </a><br>
-<a href="year/2.php"> 2nd year </a><br>
-<a href="year/3.php"> 3rd year </a><br>
-<a href="year/4.php"> 4th year </a>
+<a href="1M.php"> 1st year </a><br>
+<a href="2M.php"> 2nd year </a><br>
+<a href="3M.php"> 3rd year </a><br>
+<a href="4M.php"> 4th year </a>
 <br><br><br><br>
 <?php
 include_once("footer.php");
