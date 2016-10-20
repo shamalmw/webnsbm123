4f6981cbe9066230835edad4576fe7bc62358818
UdaraBGAkila <gamageudara77@gmail.com>
2016-10-20 23:46:06 +0530

    Update ManLec.php

--- ManLec.php
+++ ManLec.php
@@ -3,10 +3,10 @@ include_once("headerm.php");
 ?>
 <style> a {text-decoration: none; }</style>
 <br><br><br><br>
-<a href="yearL/1l.php"> 1st year </a><br>
-<a href="yearL/2l.php"> 2nd year </a><br>
-<a href="yearL/3l.php"> 3rd year </a><br>
-<a href="yearL/4l.php"> 4th year </a>
+<a href="1Ml.php"> 1st year </a><br>
+<a href="2Ml.php"> 2nd year </a><br>
+<a href="3Ml.php"> 3rd year </a><br>
+<a href="4Ml.php"> 4th year </a>
 <br><br><br><br>
 <?php
 include_once("footer.php");
