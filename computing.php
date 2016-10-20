8d6a46cd75d881dcdd8749f0aca0d570a8ee07c8
UdaraBGAkila <gamageudara77@gmail.com>
2016-10-20 23:49:34 +0530

    Update computing.php

--- computing.php
+++ computing.php
@@ -3,10 +3,10 @@ include_once("header.php");
 ?>
 <style> a {text-decoration: none; }</style>
 <br><br><br><br>
-<a href="year/1.php"> 1st year </a><br>
-<a href="year/2.php"> 2nd year </a><br>
-<a href="year/3.php"> 3rd year </a><br>
-<a href="year/4.php"> 4th year </a>
+<a href="1.php"> 1st year </a><br>
+<a href="2.php"> 2nd year </a><br>
+<a href="3.php"> 3rd year </a><br>
+<a href="4.php"> 4th year </a>
 <br><br><br><br>
 <?php
 include_once("footer.php");
