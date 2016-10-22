9287c619e582cedb9197b1962de82e0f6feeabf0
shamalmw <shamalmw27@gmail.com>
2016-10-22 23:30:28 +0530

    Add files via upload
    addmember.php

---
+++ addmember.php
@@ -0,0 +1,7 @@
+<?php?>
+<link href="addmember.css"rel="stylesheet" type="text/css">
+<form class="bb" action="form1.php"method="POST">
+full name:<br><input type="text" name="fn"placeholder="username"><br>
+password:<br><input type="PASSWORD"name="ps"placeholder="password"><br>
+<input type="submit"value="add member">
+</form>
